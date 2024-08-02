b2b3d1349d23399ae45c1397b61fb040b406bccb
wastacat <33395974+wastacat@users.noreply.github.com>
2024-08-02 16:19:43 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -36,24 +36,12 @@ def callback():
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     global working_status
+    working_status = True
     
     if event.message.type != "text":
         return
     
-    if event.message.text == "啟動":
-        working_status = True
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="我是時下流行的AI智能，目前可以為您服務囉，歡迎來跟我互動~"))
-        return
 
-    if event.message.text == "安靜":
-        working_status = False
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="感謝您的使用，若需要我的服務，請跟我說 「啟動」 謝謝~"))
-        return
-    
     if working_status:
         chatgpt.add_msg(f"Human:{event.message.text}?\n")
         reply_msg = chatgpt.get_response().replace("AI:", "", 1)
